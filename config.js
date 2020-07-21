6c5cee25e4b36a60463f5eaa12735920c6d71e95
Jing-flyloveyin <364386737@qq.com>
2020-07-21 21:43:18 +0800

    update

---
+++ config.js
@@ -0,0 +1,323 @@
+var config = {
+    style: 'mapbox://styles/mapbox/streets-v11',
+    accessToken: 'pk.eyJ1IjoibWFwYm94bWF4IiwiYSI6ImNqbnY4MHM3azA2ZmkzdnBnMThvNzRoZ28ifQ.IffqPZGkhcdPjnZ2dmSO6w',
+    showMarkers: true,
+    theme: 'light',
+    alignment: 'center',
+    title: 'Farewell Chris',
+    subtitle: 'It is lucky to have you. 感谢有你！',
+    byline: 'By Mapbox APAC Team',
+    footer: 'Source: source citations, etc.',
+    chapters: [
+        {
+            id: '1',
+            title: 'Macro',
+            image: 'Macro.jpg',
+            description: ' It is been great working with you for the past three years. I wish you all the best in your future endeavors! I miss the days of massage and drinks with you, and look forward to meeting you in Shanghai next time. I believe we both will have a wonderful future. 有缘江湖见 等风也等你！',
+            location: {
+                center: [121.45293, 31.22825],
+                zoom: 15.33,
+                pitch: 45.00,
+                bearing: 0.00
+            },
+            onChapterEnter: [
+            ],
+            onChapterExit: [
+            ]
+        },
+        {
+            id: '2',
+            title: 'Lloyd',
+            image: 'Lloyd2.jpg',
+            description: 'Thank you, Chris. Good luck for your next journey, 好想再吃一次三丰源.',
+            location: {
+                center: [121.45344, 31.22794],
+                zoom: 18.55,
+                pitch: 45.00,
+                bearing: 0.00
+            },
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: '3',
+            title: 'Mofei',
+            image: 'Mofei.jpeg',
+            description: 'Hey buddy, best wishes for your new journey, the two years of Mapboxer life is just the beginning. Don’t forget to come to China often and enjoying the best foot massage, best body massage, best 一点点, beer, dry whiskey, egg white wine… Enjoy every day!',
+            location: {
+                center: [121.45474, 31.23093],
+                zoom: 18.27,
+                pitch: 45.00,
+                bearing: 0.00
+            },
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: '4',
+            title: 'Kevin',
+            image: 'Kevin.jpg',
+            description: 'It is really a good memory and amazing journey to work with you for the past 22 months. We will miss you and look forward to reunion with you in the future.',
+            location: {
+                center: [121.45474, 31.23093],
+                zoom: 18.27,
+                pitch: 45.00,
+                bearing: 0.00
+            },
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: '5',
+            title: 'Troy',
+            image: 'Troy.jpeg',
+            description: 'Still remember the first day we talked and cannot forget how we work together to resolve issues. Best wishes to you and hope you enjoy everyday in your new journey.',
+            location: {
+                center: [114.26321, 22.33745],
+                zoom: 16.00,
+                pitch: 45.00,
+                bearing: 0.00
+            },
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: '6',
+            title: 'Joshua',
+            image: 'Joshua.jpeg',
+            description: 'It is been so lovely to work with you in the past years. All the best wishes to you!',
+            location: {
+                center: [121.23523, 31.72950],
+                zoom: 16.00,
+                pitch: 45.00,
+                bearing: 0.00
+            },
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: '7',
+            title: 'Jing',
+            image: 'Jing.jpeg',
+            description: 'Although it’s hard to believe you are going to leave, we have to say goodbye to you temporarily. It is a memorable time to work and hang out with you for the past 2 years, especially Q1-Q2, 2019 in Shanghai. May you be happy in the next career life and everything goes well! Hope to see you in person next time and have a cup of boba again.',
+            location: {
+                center: [121.21389, 31.09807],
+                zoom: 16.00,
+                pitch: 45.00,
+                bearing: 0.00
+            },
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: '8',
+            title: 'Noah',
+            image: 'Noah.jpeg',
+            description: 'Thank you for your contribution to China team and your great help to our auto team. I will miss a lot of the time you spent with us, at Shanghai office, at Xinnong‘s house, in San Francisco, and our first talk before I joined Mapbox. Keep in touch and best wishes!',
+            location: {
+                center: [121.56332, 31.14194],
+                zoom: 16.00,
+                pitch: 45.00,
+                bearing: 0.00
+            },
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: '9',
+            title: 'Jasmine',
+            image: 'Jasmine.jpeg',
+            description: 'Time flew so quickly, as I still remember that when I first joined Mapbox you as a mentor to teach me all the very important knowledge and information. You were very patient and worked through with me to where I am today! You are an amazing peer to work with!  I have to say a big “thank you!” and I’m so appreciative of all the things you’ve contributed to all of our works! I wish you all the best and good luck with your new page!',
+            location: {
+                center: [121.45796, 31.23043],
+                zoom: 17.22,
+                pitch: 45.00,
+                bearing: 0.00
+            },
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: '10',
+            title: 'Joan',
+            image: 'Joan.jpg',
+            description: 'We are so lucky to have you in our team. Thank you for everything you have done to us. Remember you always have a warm family in China. Keep in touch! We will definitely miss you and believe we will come across in the future!! And best wishes for your future endeavors and an early Happy Wedding!!!',
+            location: {
+                center: [121.59422, 31.14033],
+                zoom: 16.00,
+                pitch: 45.00,
+                bearing: 0.00
+            },
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: '11',
+            title: 'Sarah',
+            image: 'Sarah.jpeg',
+            description: 'Let us know when you come to China next time! We look forward to seeing you again in the near future. Best for your new job!',
+            location: {
+                center: [121.59422, 31.14033],
+                zoom: 16.00,
+                pitch: 45.00,
+                bearing: 0.00
+            },
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: '12',
+            title: 'Ben',
+            image: 'Ben.jpg',
+            description: 'Chris, you have been a tremendous asset to Mapbox and especially our efforts in China. You have been a driven leader focused on fixing the hardest problems and a great teammate. It’s sad to see you leave us but I am confident that you are going to be a success no matter your destination. Please reflect positively on your efforts at Mapbox, as you have had a great impact on our team, our customers and the business. All the best!',
+            location: {
+                center: [116.44111, 39.92955],
+                zoom: 16.00,
+                pitch: 45.00,
+                bearing: 0.00
+            },
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: '13',
+            title: 'Frank',
+            image: 'Frank.jpeg',
+            description: 'Thanks for all your contributions to the team, It has been a pleasure to work with you. Hope that there are some memorable moments which will accompany you for future life. All the best, and good luck for your new career!',
+            location: {
+                center: [121.57071, 31.14083],
+                zoom: 16.00,
+                pitch: 45.00,
+                bearing: 0.00
+            },
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: '14',
+            title: 'More moments',
+            image: '1.jpg',
+            description: '2019 Q1 at Jinshan(Macro‘s house)',
+            location: {
+                center: [105.30064, 34.78799],
+                zoom: 3.40,
+                pitch: 3.00,
+                bearing: 0.67
+            },
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: '15',
+            title: 'More moments',
+            image: '2.jpeg',
+            description: '2019 Q2 Have 鲜芋仙 dessert together',
+            location: {
+                center: [105.30064, 34.78799],
+                zoom: 3.40,
+                pitch: 3.00,
+                bearing: 0.67
+            },
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: '16',
+            title: 'More moments',
+            image: '4.jpeg',
+            description: '2019 Q1 Happy Valley Happy Sunday',
+            location: {
+                center: [105.30064, 34.78799],
+                zoom: 3.40,
+                pitch: 3.00,
+                bearing: 0.67
+            },
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: '17',
+            title: 'More moments',
+            image: '5.jpeg',
+            description: 'Stuck in airport with Dave',
+            location: {
+                center: [105.30064, 34.78799],
+                zoom: 3.40,
+                pitch: 3.00,
+                bearing: 0.67
+            },
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: '18',
+            title: 'More moments',
+            image: '6.jpeg',
+            description: '20190323 in Joshua‘s wedding.',
+            location: {
+                center: [105.30064, 34.78799],
+                zoom: 3.40,
+                pitch: 3.00,
+                bearing: 0.67
+            },
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: '19',
+            title: 'More moments',
+            image: '7.jpeg',
+            description: '2019 Q2 Pekking Duck at 淮海中路, Shanghai',
+            location: {
+                center: [105.30064, 34.78799],
+                zoom: 3.40,
+                pitch: 3.00,
+                bearing: 0.67
+            },
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: '20',
+            title: 'More moments',
+            image: '8.jpeg',
+            description: 'SF Sprint',
+            location: {
+                center: [105.30064, 34.78799],
+                zoom: 3.40,
+                pitch: 3.00,
+                bearing: 0.67
+            },
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: '21',
+            title: 'More moments',
+            image: '3.jpg',
+            description: '2019 Q4 Dinner with Sinomap in Beijing',
+            location: {
+                center: [105.30064, 34.78799],
+                zoom: 3.40,
+                pitch: 3.00,
+                bearing: 0.67
+            },
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: '22',
+            title: 'Great moments will never stop',
+            image: '9.jpeg',
+            description: 'May you happy weeding!!!!',
+            location: {
+                center: [105.30064, 34.78799],
+                zoom: 3.40,
+                pitch: 3.00,
+                bearing: 0.67
+            },
+            onChapterEnter: [],
+            onChapterExit: []
+        }
+    ]
+};
